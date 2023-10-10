837fef292ca0db73e79d4cb9d41d9606e729b2b8
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-10-10 23:14:18 +0800

    調整頁面配置

---
+++ commercial.js
@@ -0,0 +1,46 @@
+var slideIndex = 0;
+var touchStartX = 0;
+var touchEndX = 0;
+
+// 添加觸控事件監聽器
+var slideshow = document.querySelector(".slideshow-container");
+slideshow.addEventListener("touchstart", handleTouchStart, false);
+slideshow.addEventListener("touchend", handleTouchEnd, false);
+
+function handleTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+}
+
+function handleTouchEnd(event) {
+  touchEndX = event.changedTouches[0].clientX;
+
+  if (touchStartX - touchEndX > 20) {
+    showSlides(1); // 向左滑動，顯示下一張圖片
+  } else if (touchEndX - touchStartX > 20) {
+    showSlides(-1); // 向右滑動，顯示上一張圖片
+  }
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex += n;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// 自動輪播
+function autoSlide() {
+  showSlides(1);
+  setTimeout(autoSlide, 7000); // 每秒自動切換
+}
+
+autoSlide(); // 啟動自動輪播
